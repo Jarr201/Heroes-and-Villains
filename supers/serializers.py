6f23387d2617e653260c6c9b0c9f13049e26fdf7
Jalen Arrington <jmarrington178@yahoo.com>
2022-08-26 18:30:16 -0400

    added the FK Id Field to my serializer file

--- supers/serializers.py
+++ supers/serializers.py
@@ -6,3 +6,5 @@ class SuperSerializer(serializers.ModelSerializer):
         model = Super
         fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_types_id']
         depth = 1
+    
+    super_types_id = serializers.IntegerField(write_only=True)
